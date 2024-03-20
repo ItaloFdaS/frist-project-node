d467e1e1cdd9cbda72c8b1a33a817ae90cb47a5e
ItaloFdaS <italofelipe26@hotmail.com>
2024-03-20 19:48:51 -0300

    subindo para o github

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const uuid = require("uuid");
+
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
+});
+
+const users = [];
+
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
+
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
+
+  const user = { id: uuid.v4(), name, age };
+
+  users.push(user);
+
+  return response.status(201).json(users);
+});
+
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const { name, age } = request.body;
+
+  const updateUser = { id, name, age };
+
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index < 0) {
+    return response.status(404).json({ message: "User not found" });
+  }
+
+  users[index] = updateUser;
+
+  return response.json(updateUser);
+});
+
+app.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index < 0) {
+    return response.status(404).json({ message: "User not found" });
+  }
+
+  users.splice(index, 1);
+
+  return response.status(204).json();
+});
